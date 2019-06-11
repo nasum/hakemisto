c7df5df5c1bf32c24409f58348b1fba4c3d7a75a
nasum <tomato.wonder.life@gmail.com>
2019-06-12 02:00:14 +0900

    setup webpack

--- src/main/index.js
+++ src/main/index.js
@@ -17,7 +17,8 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  console.log(MAIN_WINDOW_WEBPACK_ENTRY)
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
