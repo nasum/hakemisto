b8b19c662f075b273b3367ed9870c6e8398cb24c
nasum <tomato.wonder.life@gmail.com>
2019-12-12 00:53:30 +0900

    reset current path

--- src/renderer/store/pages/explorer/Index.ts
+++ src/renderer/store/pages/explorer/Index.ts
@@ -1,66 +1,71 @@
-import { MutationTree, ActionTree, Module } from 'vuex'
-import { ipcRenderer } from 'electron'
+import { MutationTree, ActionTree, Module } from "vuex";
+import { ipcRenderer } from "electron";
 
-import { RootState } from '../../Index'
+import { RootState } from "../../Index";
 
 type FileType = {
-  path: string,
-  displayName: string,
-  isFile: boolean,
-  fileType: string,
-  data: string
-}
+  path: string;
+  displayName: string;
+  isFile: boolean;
+  fileType: string;
+  data: string;
+};
 
 export type ExplorerState = {
-  currentPath: string
-  fileList: FileType[]
-}
+  currentPath: string;
+  fileList: FileType[];
+};
 
 const state = (): ExplorerState => ({
-  currentPath: '',
+  currentPath: "",
   fileList: []
-})
+});
+
+const MUTATION_MAP = {
+  SET_CURRENT_PATH: "SET_CURRENT_PATH",
+  SET_FILE_LIST: "SET_FILE_LIST"
+};
 
 const actions: ActionTree<ExplorerState, RootState> = {
-  initialize({ dispatch }, path = "~/") {
-    ipcRenderer.send('getFiles', path)
-    ipcRenderer.on('receiveFiles', (event: Event, fileList: any[]) => {
-      dispatch('setFileList', fileList)
-    })
+  initialize({ commit, dispatch }, path = "~/") {
+    ipcRenderer.send("getFiles", path);
+    ipcRenderer.on("receiveFiles", (event: Event, fileList: any[]) => {
+      dispatch("setFileList", fileList);
+    });
+    commit(MUTATION_MAP.SET_CURRENT_PATH, "");
   },
   setFileList({ commit }, fileList) {
-    commit('setFileList', fileList)
+    commit(MUTATION_MAP.SET_FILE_LIST, fileList);
   },
   selectFolder({ commit }, path: string) {
-    let absolutePath = ''
+    let absolutePath = "";
     if (path === undefined) {
-      absolutePath = '~/'
+      absolutePath = "~/";
     } else {
-      absolutePath = '/' + path
+      absolutePath = "/" + path;
     }
-    ipcRenderer.send('getFiles', absolutePath)
-    commit('setCurrentPath', absolutePath)
+    ipcRenderer.send("getFiles", absolutePath);
+    commit(MUTATION_MAP.SET_CURRENT_PATH, absolutePath);
   },
   selectFile(_, path: string) {
-    ipcRenderer.send('openFile', path)
+    ipcRenderer.send("openFile", path);
   }
-}
+};
 
 const mutations: MutationTree<ExplorerState> = {
-  setCurrentPath(state: ExplorerState, path: string) {
-    state.currentPath = path
+  [MUTATION_MAP.SET_CURRENT_PATH](state: ExplorerState, path: string) {
+    state.currentPath = path;
   },
-  setFileList(state: ExplorerState, fileList: FileType[]) {
-    state.fileList = fileList
+  [MUTATION_MAP.SET_FILE_LIST](state: ExplorerState, fileList: FileType[]) {
+    state.fileList = fileList;
   }
-}
-
+};
 
 const Explorer = {
   namespaced: true,
   state: state,
   mutations: mutations,
   actions: actions
-}
+};
 
-export default Explorer
+export default Explorer;
